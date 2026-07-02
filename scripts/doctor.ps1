1ccaa4f1e9dc08a5493314284913aa2846436f73
Marcu <marcucci.francesco@itismeucci.com>
2026-07-02 22:13:17 +0200

    creato doctor.ps1

---
+++ scripts/doctor.ps1
@@ -0,0 +1,23 @@
+Write-Host "DEVOS DOCTOR"
+Write-Host "============"
+
+Write-Host "Git:"
+git --version
+
+Write-Host "Node:"
+node --version
+
+Write-Host "NPM:"
+npm --version
+
+Write-Host "PowerShell:"
+$PSVersionTable.PSVersion
+
+Write-Host "Starship:"
+starship --version
+
+Write-Host "Zoxide:"
+zoxide --version
+
+Write-Host "FZF:"
+fzf --version
